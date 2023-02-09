666ffd1a79a284a13a4dc669e65805247c891a66
oojjrs <oojjrs@gmail.com>
2023-02-09 13:17:48 +0900

    DevTool 역수입

---
+++ Assets/oojjrs/Script/Editor/ClientBridgeInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assets.Sources.Scripts.Editor
+{
+    public interface ClientBridgeInterface
+    {
+        bool Running { get; }
+
+        T AcquireObject<T>() where T : class;
+        T GetDebugEnum<T>(string name, T defValue) where T : Enum;
+        bool GetDebugFlag(string name) => GetDebugFlag(name, default);
+        bool GetDebugFlag(string name, bool defValue);
+        void Send(object packet);
+        void SetDebugEnum<T>(string name, T value) where T : Enum;
+        void SetDebugFlag(string name, bool value);
+    }
+}
---
+++ Assets/oojjrs/Script/Editor/DevTool.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace Assets.Sources.Scripts.Editor
+{
+    [ExecuteInEditMode]
+    public abstract class DevTool : EditorControl
+    {
+        public DevTool(ClientBridgeInterface clientBridge)
+            : base(clientBridge)
+        {
+
+        }
+
+        public virtual void OnDisabled()
+        {
+        }
+
+        public virtual void OnEnabled()
+        {
+        }
+
+        public void OnDraw()
+        {
+            // 어차피 별도로 처리해도 이름이 겹칠 확률은 피하기 어렵기 때문에 그냥 했다.
+            FoldoutBox(this, GetType().Name, () => GUICall(OnInnerDraw));
+        }
+
+        protected abstract void OnInnerDraw();
+    }
+}
+#endif
